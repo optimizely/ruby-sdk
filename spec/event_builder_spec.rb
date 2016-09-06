cc592a8926f7cd1841bb10a8e2bd40d345738f37
Andrew Delikat <andrew@optimizely.com>
2016-09-06 14:49:51 -0700

    Refactor tests; add expectations

--- spec/event_builder_spec.rb
+++ spec/event_builder_spec.rb
@@ -16,13 +16,11 @@ describe Optimizely::EventBuilderV2 do
     config = Optimizely::ProjectConfig.new(@config_body_JSON, @logger, @error_handler)
     bucketer = Optimizely::Bucketer.new(config)
     @event_builder = Optimizely::EventBuilderV2.new(config, bucketer)
-  end
 
-  it 'should create a valid V2 Event when create_impression_event is called' do
     time_now = Time.now
     allow(Time).to receive(:now).and_return(time_now)
 
-    expected_params = {
+    @expected_impression_params = {
       'visitorId' => 'testUser',
       'timestamp' => (time_now.to_f * 1000).to_i,
       'isGlobalHoldback' => false,
@@ -38,155 +36,93 @@ describe Optimizely::EventBuilderV2 do
       'clientVersion' => Optimizely::VERSION,
       'userFeatures' => [],
     }
+
+    @expected_conversion_params = {
+      'visitorId' => 'testUser',
+      'timestamp' => (time_now.to_f * 1000).to_i,
+      'isGlobalHoldback' => false,
+      'projectId' => '111001',
+      'accountId' => '12001',
+      'clientEngine' => 'ruby-sdk',
+      'clientVersion' => Optimizely::VERSION,
+      'userFeatures' => [],
+      'eventMetrics' => [],
+      'eventFeatures' => [],
+      'eventName' => 'testEvent',
+      'eventEntityId' => '111095',
+      'layerStates' => [{
+        'layerId' => '4',
+        'decision' => {
+          'variationId' => '111128',
+          'experimentId' => '111127',
+          'isLayerHoldback' => false,
+        },
+        'actionTriggered' => true,
+      }],
+    }
+  end
+
+  it 'should create a valid V2 Event when create_impression_event is called' do
     expected_url = 'https://p13nlog.dz.optimizely.com/log/decision'
 
     impression_event = @event_builder.create_impression_event('testExperiment', '111128', 'testUser', nil)
-    expect(impression_event.params).to eq(expected_params)
+    expect(impression_event.params).to eq(@expected_impression_params)
     expect(impression_event.url).to eq(expected_url)
     expect(impression_event.http_verb).to eq(:post)
   end
 
   it 'should create a valid V2 Event when create_impression_event is called with attributes' do
-    time_now = Time.now
-    allow(Time).to receive(:now).and_return(time_now)
-
-    expected_params = {
-      'visitorId' => 'testUser',
-      'timestamp' => (time_now.to_f * 1000).to_i,
-      'isGlobalHoldback' => false,
-      'projectId' => '111001',
-      'decision' => {
-        'variationId' => '111128',
-        'experimentId' => '111127',
-        'isLayerHoldback' => false,
-      },
-      'layerId' => '4',
-      'accountId' => '12001',
-      'clientEngine' => 'ruby-sdk',
-      'clientVersion' => Optimizely::VERSION,
-      'userFeatures' => [{
-        'id' => '111094',
-        'name' => 'browser_type',
-        'type' => 'custom',
-        'value' => 'firefox',
-        'shouldIndex' => true,
-      }],
-    }
     expected_url = 'https://p13nlog.dz.optimizely.com/log/decision'
+    @expected_impression_params['userFeatures'] = [{
+      'id' => '111094',
+      'name' => 'browser_type',
+      'type' => 'custom',
+      'value' => 'firefox',
+      'shouldIndex' => true,
+    }]
 
     impression_event = @event_builder.create_impression_event('testExperiment', '111128', 'testUser', {'browser_type' => 'firefox'})
-    expect(impression_event.params).to eq(expected_params)
+    expect(impression_event.params).to eq(@expected_impression_params)
+    expect(impression_event.url).to eq(expected_url)
+    expect(impression_event.http_verb).to eq(:post)
   end
 
   it 'should create a valid V2 Event when create_conversion_event is called' do
-    time_now = Time.now
-    allow(Time).to receive(:now).and_return(time_now)
-
-    expected_params = {
-      'visitorId' => 'testUser',
-      'timestamp' => (time_now.to_f * 1000).to_i,
-      'isGlobalHoldback' => false,
-      'projectId' => '111001',
-      'accountId' => '12001',
-      'clientEngine' => 'ruby-sdk',
-      'clientVersion' => Optimizely::VERSION,
-      'userFeatures' => [],
-      'eventMetrics' => [],
-      'eventFeatures' => [],
-      'eventName' => 'testEvent',
-      'eventEntityId' => '111095',
-      'layerStates' => [{
-        'layerId' => '4',
-        'decision' => {
-          'variationId' => '111128',
-          'experimentId' => '111127',
-          'isLayerHoldback' => false,
-        },
-        'actionTriggered' => true,
-      }],
-    }
-
     expected_url = 'https://p13nlog.dz.optimizely.com/log/event'
 
     conversion_event = @event_builder.create_conversion_event('testEvent', 'testUser', nil, nil, ['testExperiment'])
-    expect(conversion_event.params).to eq(expected_params)
+    expect(conversion_event.params).to eq(@expected_conversion_params)
     expect(conversion_event.url).to eq(expected_url)
     expect(conversion_event.http_verb).to eq(:post)
   end
 
   it 'should create a valid V2 Event when create_conversion_event is called with attributes' do
-    time_now = Time.now
-    allow(Time).to receive(:now).and_return(time_now)
-
-    expected_params = {
-      'visitorId' => 'testUser',
-      'timestamp' => (time_now.to_f * 1000).to_i,
-      'isGlobalHoldback' => false,
-      'projectId' => '111001',
-      'accountId' => '12001',
-      'clientEngine' => 'ruby-sdk',
-      'clientVersion' => Optimizely::VERSION,
-      'userFeatures' => [{
-        'id' => '111094',
-        'name' => 'browser_type',
-        'type' => 'custom',
-        'value' => 'firefox',
-        'shouldIndex' => true,
-      }],
-      'eventMetrics' => [],
-      'eventFeatures' => [],
-      'eventName' => 'testEvent',
-      'eventEntityId' => '111095',
-      'layerStates' => [{
-        'layerId' => '4',
-        'decision' => {
-          'variationId' => '111128',
-          'experimentId' => '111127',
-          'isLayerHoldback' => false,
-        },
-        'actionTriggered' => true,
-      }],
-    }
-
     expected_url = 'https://p13nlog.dz.optimizely.com/log/event'
+    @expected_conversion_params['userFeatures'] = [{
+      'id' => '111094',
+      'name' => 'browser_type',
+      'type' => 'custom',
+      'value' => 'firefox',
+      'shouldIndex' => true,
+    }]
 
     conversion_event = @event_builder.create_conversion_event('testEvent', 'testUser', {'browser_type' => 'firefox'}, nil, ['testExperiment'])
+    expect(conversion_event.params).to eq(@expected_conversion_params)
+    expect(conversion_event.url).to eq(expected_url)
+    expect(conversion_event.http_verb).to eq(:post)
   end
 
   it 'should create a valid V2 Event when create_conversion_event is called with event value' do
-    time_now = Time.now
-    allow(Time).to receive(:now).and_return(time_now)
-
-    expected_params = {
-      'visitorId' => 'testUser',
-      'timestamp' => (time_now.to_f * 1000).to_i,
-      'isGlobalHoldback' => false,
-      'projectId' => '111001',
-      'accountId' => '12001',
-      'clientEngine' => 'ruby-sdk',
-      'clientVersion' => Optimizely::VERSION,
-      'userFeatures' => [],
-      'eventMetrics' => [{
-        'name' => 'revenue',
-        'value' => 4200,
-      }],
-      'eventFeatures' => [],
-      'eventName' => 'testEvent',
-      'eventEntityId' => '111095',
-      'layerStates' => [{
-        'layerId' => '4',
-        'decision' => {
-          'variationId' => '111128',
-          'experimentId' => '111127',
-          'isLayerHoldback' => false,
-        },
-        'actionTriggered' => true,
-      }],
-    }
-
     expected_url = 'https://p13nlog.dz.optimizely.com/log/event'
+    @expected_conversion_params['eventMetrics'] = [{
+      'name' => 'revenue',
+      'value' => 4200,
+    }]
 
     conversion_event = @event_builder.create_conversion_event('testEvent', 'testUser', nil, 4200, ['testExperiment'])
+    expect(conversion_event.params).to eq(@expected_conversion_params)
+    expect(conversion_event.url).to eq(expected_url)
+    expect(conversion_event.http_verb).to eq(:post)
   end
 end
 
