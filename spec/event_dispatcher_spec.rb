cc8539dbdc8fcaed2a7b4edc5a3f3b0649c746d3
Andrew Delikat <andrew@optimizely.com>
2016-09-06 16:47:25 -0700

    Add test for V2 events

--- spec/event_dispatcher_spec.rb
+++ spec/event_dispatcher_spec.rb
@@ -18,11 +18,20 @@ describe Optimizely::EventDispatcher do
     @event_dispatcher = Optimizely::EventDispatcher.new
   end
 
-  it 'should fire off GET request with provided URL and params' do
+  it 'should properly dispatch V1 (GET) events' do
     stub_request(:get, @url).with(:query => @params)
     event = Optimizely::Event.new(:get, @url, @params)
     @event_dispatcher.dispatch_event(event)
 
     expect(a_request(:get, @url).with(:query => @params)).to have_been_made.once
+  end
+
+  it 'should properly dispatch V2 (POST) events' do
+    stub_request(:post, @url)
+    event = Optimizely::Event.new(:post, @url, @params)
+    @event_dispatcher.dispatch_event(event)
+
+    expect(a_request(:post, @url).
+      with(:body => @params, :headers => {'Content-Type' => 'application/json'})).to have_been_made.once
   end
 end
