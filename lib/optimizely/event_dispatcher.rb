29abfc6b06783818ddcc5b754818af4dba17ec3b
Andrew Delikat <andrew@optimizely.com>
2016-09-29 14:52:33 -0700

    Revert "Remove rescues from EventDispatcher"
    This reverts commit 84891e525676001027a3779b51c1967369264838.

--- lib/optimizely/event_dispatcher.rb
+++ lib/optimizely/event_dispatcher.rb
@@ -17,12 +17,20 @@ module Optimizely
       # event - Event object
 
       if event.http_verb == :get
-        HTTParty.get(event.url, headers: event.headers, query: event.params, timeout: REQUEST_TIMEOUT)
+        begin
+          HTTParty.get(event.url, headers: event.headers, query: event.params, timeout: REQUEST_TIMEOUT)
+        rescue Timeout::Error => e
+          return e
+        end
       elsif event.http_verb == :post
-        HTTParty.post(event.url,
-                 body: event.params.to_json,
-                 headers: event.headers,
-                 timeout: REQUEST_TIMEOUT)
+        begin
+          HTTParty.post(event.url,
+                   body: event.params.to_json,
+                   headers: event.headers,
+                   timeout: REQUEST_TIMEOUT)
+        rescue Timeout::Error => e
+          return e
+        end
       end
     end
   end
