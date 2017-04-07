d2371c36bb3bcdedfe3739b3fbd9982b93475787
Roman Onufryk <roman@optimizely.com>
2017-04-06 18:41:07 -0700

    Covered with new tests, wip

--- spec/bucketing_spec.rb
+++ spec/bucketing_spec.rb
@@ -52,20 +52,6 @@ describe Optimizely::Bucketer do
       'a very very very very very very very very very very very very very very very long ppd string'))).to eq(6128)
   end
 
-  it 'should return correct variation ID if user ID is in forcedVariations and variation is valid' do
-    expect(bucketer.bucket('test_experiment', 'forced_user1')).to eq('111128')
-    expect(spy_logger).to have_received(:log)
-                      .once.with(Logger::INFO, "User 'forced_user1' is forced in variation 'control'.")
-
-    expect(bucketer.bucket('test_experiment', 'forced_user2')).to eq('111129')
-    expect(spy_logger).to have_received(:log)
-                      .once.with(Logger::INFO, "User 'forced_user2' is forced in variation 'variation'.")
-  end
-
-  it 'should return null if forced variation ID is not in the datafile' do
-    expect(bucketer.bucket('test_experiment', 'forced_user_with_invalid_variation')).to be_nil
-  end
-
   it 'should return the proper variation for a user in a mutually exclusive grouped experiment' do
     expect(bucketer).to receive(:generate_bucket_value).twice.and_return(3000)
 
@@ -98,14 +84,6 @@ describe Optimizely::Bucketer do
     expect(bucketer.bucket('group1_exp2', 'test_user')).to be_nil
     expect(spy_logger).to have_received(:log)
                             .with(Logger::INFO, "User 'test_user' is in no experiment.")
-  end
-
-  it 'should respect forced variations within mutually exclusive grouped experiments' do
-    expect(bucketer).not_to receive(:generate_bucket_value)
-
-    expect(bucketer.bucket('group1_exp2', 'forced_group_user1')).to eq('130004')
-    expect(spy_logger).to have_received(:log)
-                      .once.with(Logger::INFO, "User 'forced_group_user1' is forced in variation 'g1_e2_v2'.")
   end
 
   it 'should return the proper variation for a user in an overlapping grouped experiment' do
@@ -149,6 +127,32 @@ describe Optimizely::Bucketer do
                       .with(Logger::INFO, "User 'test_user' is in no variation.")
     expect(spy_logger).to have_received(:log)
                       .with(Logger::DEBUG, "Bucketed into an empty traffic range. Returning nil.")
+  end
+
+  describe '#get_forced_variation_id' do
+    it 'should return correct variation ID if user ID is in forcedVariations and variation is valid' do
+      expect(bucketer.get_forced_variation_id('test_experiment', 'forced_user1')).to eq('111128')
+      expect(spy_logger).to have_received(:log)
+                              .once.with(Logger::INFO, "User 'forced_user1' is forced in variation 'control'.")
+
+      expect(bucketer.get_forced_variation_id('test_experiment', 'forced_user2')).to eq('111129')
+      expect(spy_logger).to have_received(:log)
+                              .once.with(Logger::INFO, "User 'forced_user2' is forced in variation 'variation'.")
+    end
+
+    it 'should return null if forced variation ID is not in the datafile' do
+      expect(bucketer.get_forced_variation_id('test_experiment', 'forced_user_with_invalid_variation')).to be_nil
+    end
+
+    it 'should respect forced variations within mutually exclusive grouped experiments' do
+      expect(bucketer).not_to receive(:generate_bucket_value)
+
+      expect(bucketer.get_forced_variation_id('group1_exp2', 'forced_group_user1')).to eq('130004')
+      expect(spy_logger).to have_received(:log)
+                              .once.with(Logger::INFO, "User 'forced_group_user1' is forced in variation 'g1_e2_v2'.")
+    end
+
+
   end
 
   describe 'logging' do
