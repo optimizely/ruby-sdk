20495aafc3b0b649dba72493b6a0cde2c444dccf
Matt Auerbach <matthew.auerbach@optimizely.com>
2017-09-21 10:56:06 -0700

    formatting

--- lib/optimizely/event_builder.rb
+++ lib/optimizely/event_builder.rb
@@ -132,9 +132,8 @@ module Optimizely
       return if attributes.nil?
 
       attributes.keys.each do |attribute_key|
-        # Omit falsy attribute values
+        # Omit falsy or nil attribute values
         attribute_value = attributes[attribute_key]
-
         next unless attribute_value
 
         # Skip attributes not in the datafile
