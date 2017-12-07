70fae669af34a79ff76951bbb6bb0a7ac6186972
rashidsp <rashidsp204@gmail.com>
2017-12-07 23:16:24 +0500

    fixes tests issues

--- demo/test/controllers/demo_controller_test.rb
+++ demo/test/controllers/demo_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class DemoControllerTest < ActionDispatch::IntegrationTest
   test 'should get new' do
-    get demo_config_new_url
+    get demo_config_url
     assert_response :success
   end
 end
