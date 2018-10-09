c602b9aa283576548300fc1275130aac141048f3
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2018-10-09 23:25:29 +0000

    parametrize SDK again

--- ci/trigger_build.sh
+++ ci/trigger_build.sh
@@ -26,8 +26,8 @@ body=$(cat <<EOF
           "PERF_NUM_RUNS": 50,
           "RESULTS_DIR": "./test_results",
           "TESTAPP_PORT_BINDING": 3000,
-          "SDK": "ruby",
-          "COMPOSE_PROJECT_NAME": "fullstack-compat-${TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER}-ruby"
+          "SDK": "${SDK}",
+          "COMPOSE_PROJECT_NAME": "fullstack-compat-${TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER}-${SDK}"
         }
       },
       "before_script": "STATE=pending ci/update_build_status.sh",
