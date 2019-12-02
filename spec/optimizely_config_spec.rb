e984dc6cf7df78b5afc984681a88cfdafbb3580c
Zeeshan Ashraf <zashraf@folio3.com>
2019-12-02 10:44:20 +0500

    unit tests complete

--- spec/optimizely_config_spec.rb
+++ spec/optimizely_config_spec.rb
@@ -18,7 +18,7 @@
 
 require 'spec_helper'
 
-describe Optimizely::OptimizelyConfig, :focus => true do
+describe Optimizely::OptimizelyConfig do
   let(:config_body_JSON) { OptimizelySpec::VALID_CONFIG_BODY_JSON }
   let(:config_typed_audience_JSON) { JSON.dump(OptimizelySpec::CONFIG_DICT_WITH_TYPED_AUDIENCES) }
   let(:error_handler) { Optimizely::NoOpErrorHandler.new }
@@ -48,11 +48,50 @@ describe Optimizely::OptimizelyConfig, :focus => true do
   it 'should return all feature flags' do
     features_map = optimizely_config['featuresMap']
     expect(features_map.length).to eq(8)
+    project_config.feature_flags.each do |feature_flag|
+      expect(features_map[feature_flag['key']]).to include(
+        'id' => feature_flag['id'],
+        'key' => feature_flag['key']
+      )
+      experiments_map = features_map[feature_flag['key']]['experimentsMap']
+      feature_flag['experimentIds'].each do |experiment_id|
+        experiment_key = project_config.get_experiment_key(experiment_id)
+        expect(experiments_map[experiment_key]).to be_truthy
+      end
+      variables_map = features_map[feature_flag['key']]['variablesMap']
+      feature_flag['variables'].each do |variable|
+        expect(variables_map[variable['key']]).to include(
+          'id' => variable['id'],
+          'key' => variable['key'],
+          'type' => variable['type'],
+          'value' => variable['defaultValue']
+        )
+      end
+    end
   end
 
   it 'should correctly merge all feature variables' do
+    project_config.feature_flags.each do |feature_flag|
+      feature_flag['experimentIds'].each do |experiment_id|
+        experiment = project_config.experiment_id_map[experiment_id]
+        variations = experiment['variations']
+        variations_map = optimizely_config['experimentsMap'][experiment['key']]['variationsMap']
+        variations.each do |variation|
+          feature_flag['variables'].each do |variable|
+            variable_to_assert = variations_map[variation['key']]['variablesMap'][variable['key']]
+            expect(variable).to include(
+              'id' => variable_to_assert['id'],
+              'key' => variable_to_assert['key'],
+              'type' => variable_to_assert['type']
+            )
+            expect(variable['defaultValue']).to eq(variable_to_assert['value']) unless variation['featureEnabled']
+          end
+        end
+      end
+    end
   end
 
   it 'should return correct config revision' do
+    expect(project_config.revision).to eq(optimizely_config['revision'])
   end
 end
