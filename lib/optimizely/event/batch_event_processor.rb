bd1acff8d26bf443e1bbba0a1da624aac94d92ba
Thomas Zurkan <thomas.zurkan@optimizely.com>
2019-12-04 11:25:52 -0800

    sleep interval in seconds

--- lib/optimizely/event/batch_event_processor.rb
+++ lib/optimizely/event/batch_event_processor.rb
@@ -122,7 +122,7 @@ module Optimizely
         item = @event_queue.pop if @event_queue.length.positive? || @nil_count > MAX_NIL_COUNT
 
         if item.nil?
-          interval = @flushing_interval_deadline - Helpers::DateTimeUtils.create_timestamp
+          interval = (@flushing_interval_deadline - Helpers::DateTimeUtils.create_timestamp)/1000
           @logger.log(
               Logger::DEBUG,
               'Sleeping for ' + interval.to_s
