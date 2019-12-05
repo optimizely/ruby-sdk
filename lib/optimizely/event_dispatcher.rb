3e645764907556ef755d2fe697edb1ba176a4bec
Owais Akbani <owais.akbani92@gmail.com>
2019-12-05 07:26:48 +0500

    feat: Log and Handle Error for Event Dispatcher response (#221)
    * WIP
    
    * finalize

--- lib/optimizely/event_dispatcher.rb
+++ lib/optimizely/event_dispatcher.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 #
-#    Copyright 2016-2017, Optimizely and contributors
+#    Copyright 2016-2017, 2019, Optimizely and contributors
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -15,6 +15,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
+require_relative 'exceptions'
+
 require 'httparty'
 
 module Optimizely
@@ -28,26 +30,46 @@ module Optimizely
     # @api constants
     REQUEST_TIMEOUT = 10
 
+    def initialize(logger: nil, error_handler: nil)
+      @logger = logger || NoOpLogger.new
+      @error_handler = error_handler || NoOpErrorHandler.new
+    end
+
     # Dispatch the event being represented by the Event object.
     #
     # @param event - Event object
     def dispatch_event(event)
       if event.http_verb == :get
-        begin
-          HTTParty.get(event.url, headers: event.headers, query: event.params, timeout: REQUEST_TIMEOUT)
-        rescue Timeout::Error => e
-          return e
-        end
+        response = HTTParty.get(event.url, headers: event.headers, query: event.params, timeout: REQUEST_TIMEOUT)
+
       elsif event.http_verb == :post
-        begin
-          HTTParty.post(event.url,
-                        body: event.params.to_json,
-                        headers: event.headers,
-                        timeout: REQUEST_TIMEOUT)
-        rescue Timeout::Error => e
-          return e
-        end
+        response = HTTParty.post(event.url,
+                                 body: event.params.to_json,
+                                 headers: event.headers,
+                                 timeout: REQUEST_TIMEOUT)
       end
+
+      error_msg = "Event failed to dispatch with response code: #{response.code}"
+
+      case response.code
+      when 400...500
+        @logger.log(Logger::ERROR, error_msg)
+        @error_handler.handle_error(HTTPCallError.new("HTTP Client Error: #{response.code}"))
+
+      when 500...600
+        @logger.log(Logger::ERROR, error_msg)
+        @error_handler.handle_error(HTTPCallError.new("HTTP Server Error: #{response.code}"))
+      end
+    rescue Timeout::Error => e
+      @logger.log(Logger::ERROR, "Request Timed out. Error: #{e}")
+      @error_handler.handle_error(e)
+
+      # Returning Timeout error to retain existing behavior.
+      e
+    rescue StandardError => e
+      @logger.log(Logger::ERROR, "Event failed to dispatch. Error: #{e}")
+      @error_handler.handle_error(e)
+      nil
     end
   end
 end
