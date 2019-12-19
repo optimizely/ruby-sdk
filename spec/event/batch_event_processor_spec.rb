6f5952a4fc29e11dad754885ab4cc06961be13db
Thomas Zurkan <thomas.zurkan@optimizely.com>
2019-12-19 11:10:48 -0800

    trying to get 2.4.4 queue to fill

--- spec/event/batch_event_processor_spec.rb
+++ spec/event/batch_event_processor_spec.rb
@@ -344,7 +344,7 @@ describe Optimizely::BatchEventProcessor do
     )
 
     user_event = Optimizely::UserEventFactory.create_conversion_event(project_config, event, 'test_user', nil, nil)
-    100.times do
+    900.times do
       @event_processor.process(user_event)
     end
 
