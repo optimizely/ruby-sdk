e92694e73fe0579461c02753d5b714a3e8ed85d6
Zeeshan Ashraf <zashraf@folio3.com>
2020-01-16 19:28:18 +0500

    updated copyright

--- lib/optimizely.rb
+++ lib/optimizely.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 #
-#    Copyright 2016-2019, Optimizely and contributors
+#    Copyright 2016-2020, Optimizely and contributors
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
