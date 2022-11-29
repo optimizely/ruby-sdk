1636aff16f909998743595c38494a0b88321e121
Ozayr <54209343+ozayr-zaviar@users.noreply.github.com>
2022-11-29 20:00:18 +0500

    lint fix

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4567,6 +4567,7 @@ describe 'Optimizely' do
     it 'should accept valid custom event manager' do
       class CustomEventManager # rubocop:disable Lint/ConstantDefinitionInBlock
         attr_accessor :odp_event_timeout
+
         def send_event(extra_param = nil, action:, type:, identifiers:, data:, other_extra_param: 'great'); end
         def start!(odp_config); end
         def update_config; end
