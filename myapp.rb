4de93c8194aa2f6655db94cc115fd8a77e941a02
Matjaz Pirnovar <Mat001@users.noreply.github.com>
2023-01-19 17:06:30 -0800

    add opti config, remove sleep time

--- myapp.rb
+++ myapp.rb
@@ -6,15 +6,13 @@ require "logger"
 # Initialize an Optimizely client
 optimizely_client = Optimizely::OptimizelyFactory.custom_instance('TbrfRLeKvLyWGusqANoeR', nil, nil, logger=Optimizely::SimpleLogger.new(Logger::INFO))
 
-# config = optimizely_client.get_optimizely_config
+config = optimizely_client.get_optimizely_config
 
 attributes = {"laptop_os" => "mac"}
 
 # user = optimizely_client.create_user_context('12345', attributes)    # regular attributes, user context takes any user id - important when running these 3 lines I need to adjust the audience in the app (use the correct one)!!!
 user = optimizely_client.create_user_context('fs-id-12', attributes)    # prebuilt segments (no lists), valid user id is fs-id-12 (matches DOB)
 # user = optimizely_client.create_user_context('fs-id-6', attributes)    # list segment, valid use is fs-id-6
-
-sleep(1)    # sleep is needed for datafile to load - check this, might be an issue
 
 user.fetch_qualified_segments()
 segments = user.qualified_segments()
