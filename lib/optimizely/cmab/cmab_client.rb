1473c2ecf3911b654b5ed856e65af8df1db9c668
esrakartalOpt <esra.kartal@optimizely.com>
2025-06-11 16:33:18 -0500

    add begin

--- lib/optimizely/cmab/cmab_client.rb
+++ lib/optimizely/cmab/cmab_client.rb
@@ -151,7 +151,7 @@ module Optimizely
       #   The variation ID from the response.
 
       backoff = retry_config.initial_backoff
-      (0..retry_config.max_retries).each do |attempt|
+      begin
         _do_fetch(url, request_body, timeout)
       rescue StandardError => e
         if attempt < retry_config.max_retries
