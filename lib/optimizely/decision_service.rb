ccc93d160c13dd91be339a8e7c485c45bfdb9c8b
esrakartalOpt <esra.kartal@optimizely.com>
2025-12-03 13:37:11 -0600

    Test FSC

--- lib/optimizely/decision_service.rb
+++ lib/optimizely/decision_service.rb
@@ -313,12 +313,18 @@ module Optimizely
       decisions = []
       feature_flags.each do |feature_flag|
         # check if the feature is being experiment on and whether the user is bucketed into the experiment
-        decision_result = get_variation_for_feature_experiment(project_config, feature_flag, user_context, user_profile_tracker, decide_options)
-        # Only process rollout if no experiment decision was found and no error
-        if decision_result.decision.nil? && !decision_result.error
-          decision_result_rollout = get_variation_for_feature_rollout(project_config, feature_flag, user_context) unless decision_result.decision
-          decision_result.decision = decision_result_rollout.decision
-          decision_result.reasons.push(*decision_result_rollout.reasons)
+        holdouts = project_config.get_holdouts_for_flag(feature_flag['id'])
+        
+        if holdouts && !holdouts.empty?
+          decision_result = get_decision_for_flag(feature_flag, user_context, project_config, decide_options, user_profile_tracker)
+        else
+          decision_result = get_variation_for_feature_experiment(project_config, feature_flag, user_context, user_profile_tracker, decide_options)
+          # Only process rollout if no experiment decision was found and no error
+          if decision_result.decision.nil? && !decision_result.error
+            decision_result_rollout = get_variation_for_feature_rollout(project_config, feature_flag, user_context) unless decision_result.decision
+            decision_result.decision = decision_result_rollout.decision
+            decision_result.reasons.push(*decision_result_rollout.reasons)
+          end
         end
         decisions << decision_result
       end
